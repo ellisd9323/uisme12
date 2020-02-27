90cc216d0e9b2339784dcaa384026e27d45d2c46
ellisd9323 <61558886+ellisd9323@users.noreply.github.com>
2020-02-27 13:50:10 -0500

    Add files via upload

---
+++ ellisdukeP2T1.py
@@ -0,0 +1,15 @@
+#2/27/20
+#CTI-110 P2T1 - Sales Prediction
+# Duke Ellis
+
+#Get the projected total sales
+total_sales = float (input('Enter the projected sales:'))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales = 0.23
+
+#Calculate the profit 0f 23 percent total sales
+profit = total_sales = 0.23
+
+#display the profit
+print ('the profit is $' , format(profit,',.2f'))
